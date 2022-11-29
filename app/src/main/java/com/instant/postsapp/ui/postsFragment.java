2ee6da9f05029425df630916399f1adb20a1e759
nourheta <nourheta44@gmail.com>
2022-11-29 23:58:17 +0200

    adding navigation component

---
+++ app/src/main/java/com/instant/postsapp/ui/postsFragment.java
@@ -0,0 +1,44 @@
+package com.instant.postsapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.instant.postsapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link postsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class postsFragment extends Fragment {
+
+
+    public postsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
